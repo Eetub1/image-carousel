e70e51e4812665b967b5207f8aa3576c6b3979a1
eeeeoino <eetu.oinonen04@gmail.com>
2025-07-17 10:53:25 +0300

    Made a neat little image carousel

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
-import './style.css';
 
-console.log('Webpack is working!');
+const carImages = document.querySelectorAll(".carImg")
+const navBallsCont = document.getElementById("balls")
+let index = 0
+
+function colorBall() {
+    const balls = document.querySelectorAll(".navBall")
+    for (const ball of balls) {
+        ball.classList.remove("selected")
+    }
+    balls[index].classList.add("selected")
+}
+
+function prevSlide() {
+    index -= 1
+    if (index < 0) {
+        index = carImages.length - 1
+    }
+    showSlide()
+}
+
+function nextSlide() {
+    index += 1
+    if (index >= carImages.length) {
+        index = 0
+    }
+    showSlide()
+}
+
+function showSlide() {
+    for (let i = 0; i < carImages.length; i++) {
+        carImages[i].classList.add("hide")
+        carImages[i].classList.remove("show")
+    }
+
+    colorBall()
+
+    carImages[index].classList.remove("hide")
+    carImages[index].classList.add("show")
+}
+
+function createNavBalls() {
+    for (let i = 0; i < carImages.length; i++) {
+        const btn = document.createElement("button")
+        btn.addEventListener("click", () => {
+            index = i
+            showSlide()
+        })
+        btn.className = "navBall"
+        navBallsCont.appendChild(btn)
+    }
+}
+
+createNavBalls()
+showSlide()
